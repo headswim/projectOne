573b947f51fcd784dcfa7eb154e9f345152a597e
Harrison Dimon <harrison.dimon@gmail.com>
2016-07-08 21:27:53 -0400

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,2 +1,7 @@
+//javascript
+var js = 33;
 
-var js = 33
+var randomNum = Math.floor((Math.random() * 10) + 1);
+
+var el = document.getElementById('info');
+//el.innerHTML = '<h2>random number</h2><p>' + randomNum + '</p>';
